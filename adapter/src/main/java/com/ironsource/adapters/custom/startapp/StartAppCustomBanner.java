b6447166eede91c184e2fe5c47ba7548b127475a
Radmyr Tsyhulksyi <radmyr.tsyhulskyi@start.io>
2023-11-08 22:34:47 +0200

    version 1.0.5: -consent disabled by defaulti - return and splash ad disabled by default

--- adapter/src/main/java/com/ironsource/adapters/custom/startapp/StartAppCustomBanner.java
+++ adapter/src/main/java/com/ironsource/adapters/custom/startapp/StartAppCustomBanner.java
@@ -7,7 +7,6 @@ import static com.ironsource.mediationsdk.adunit.adapter.utility.AdapterErrors.A
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
